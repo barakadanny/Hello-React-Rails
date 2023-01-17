88d0540539debf3c27c074c0ac43c2e37cdd59fd
Danny <barakadan421@gmail.com>
2023-01-17 23:38:21 +0300

    Update store

--- app/javascript/store.js
+++ app/javascript/store.js
@@ -1,10 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { greetingSlice } from './greetingSlice'
 
-const initialState = {
-  message: ''
-};
-
 const store = configureStore({
   reducer: {
     greeting: greetingSlice
