c3aed88460a7a9956fded7e13cda98e86c6f8b45
Danny <barakadan421@gmail.com>
2023-01-17 17:44:56 +0300

    call components in the app

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+// import "@hotwired/turbo-rails"
+// import "./controllers"
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Greetings from './components/Greetings';
+
+ReactDOM.render(
+  <Greetings/>,
+  document.getElementById('root'),
+);
