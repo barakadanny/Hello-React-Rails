abfca739d8a6ec3c6ae620f9e61f5060ada8d612
Danny <barakadan421@gmail.com>
2023-01-17 23:07:54 +0300

    App react js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,9 +5,13 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import Greetings from './components/Greetings';
 
 ReactDOM.render(
-  <Greetings/>,
+    <Provider store={store}>
+        <Greetings/>
+    </Provider>,
   document.getElementById('root'),
 );
