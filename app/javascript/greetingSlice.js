0654faa2a590b1385d9c5b3e44a36e113e5ead61
Danny <barakadan421@gmail.com>
2023-01-21 15:36:40 +0300

    GreetingSlice update

--- app/javascript/greetingSlice.js
+++ app/javascript/greetingSlice.js
@@ -1,32 +1,23 @@
 import { createAsyncThunk ,createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-    message: ''
-};
-
 export const fetchGreetings = createAsyncThunk(
-    'greeting/fetchGreeting',
+    'greeting/fetchGreetings',
     async () => {
         const response = await fetch('http://127.0.0.1:3000/api/v1/greetings/random');
         const data = await response.json();
-        return data.message;
+        return data;
     }
 );
 
-export const greetingSlice = createSlice({
+const greetingSlice = createSlice({
     name: 'greeting',
-    initialState,
-    reducers: {
-        fetchGreeting: (state, action) => {
-            state.message = action.payload;
+    initialState: { message: "" },
+    
+    extraReducers: {
+        [fetchGreetings.fulfilled]: (state, action) => {
+            state.message = action.payload.message;
         }
-    },
-    extraReducers: (builder) => {
-        builder
-            .addCase(fetchGreetings.fulfilled, (state, action) => {
-                state.message = action.payload;
-            });
-    }
+      },
 });
 
 export const { fetchGreeting } = greetingSlice.actions;
